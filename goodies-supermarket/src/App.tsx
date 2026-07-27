49828dd41e663896e5d6262b71ca2a6fabf01367
FRANKLYN NGAUJAH <you@example.com>
2026-07-27 11:12:39 +0000

    project setup

---
+++ goodies-supermarket/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-green-600 text-5xl font-bold text-white">
+      Goodies Supermarket
+    </div>
+  );
+}
+
+export default App;
